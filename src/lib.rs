7654c75c4ccb4df20c07aa56b4bcd88cd2268ad3
rustmadeeasy-medium <rustmadeeasy@proton.me>
2024-12-18 22:27:21 +0100

    adding text for variables

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,17 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <div> 
-            <h1> {"Hello World !! "} </h1>
-            <h2> {"I love Rust "} </h2> 
+            <div class="var-container">
+                <p> {"Variables"} </p>
+            </div>
+            <div class = "var-content"> 
+                <p> {"let"} </p> 
+                <div class = "ref"> {"Reference"} </div>
+                <div class = "mut"> {"Mutability"} </div>
+                <div class = "type"> {"Type"} </div>
+            </div>
+            <p> {"="} </p>
+            <p> {"value"} </p>
         </div>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,3 @@ use mindmap_rust::App;
 fn main() {
     yew::start_app::<App>();
 }
-
-// test
